e01e62281a088ac64e8662013506f8726f282c41
Rof13thfloor <mojtaba.moazen.a@gmail.com>
2024-02-19 18:12:36 +0100

    Crawler changed to get most downloaded nodes

--- crawler.py
+++ crawler.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager 
 from selenium.webdriver.common.by import By
 from collections import Counter 
+import re 
 
 nodes = []
 
@@ -14,16 +15,26 @@ def CrawlFlowPage(urls):
         driver.get(link) 
         time.sleep(5)
         elements = driver.find_elements(By.CLASS_NAME ,"nodeTypeList")
+        li_elements = []
         if (len(elements)!=0):
-            li_elements = elements[0].find_elements(By.TAG_NAME, "li")
+            for l in elements:
+                li_elements.extend(l.find_elements(By.TAG_NAME, "li"))
+        total_node_number = 0 
         for li in li_elements:
             try:
-                nodes.append(li.text.split(" ")[0].strip(" "))
+                
+                node_number = int(re.findall(r'\d+',li.text.split(" ")[-1].split("x")[-1])[0])
+                total_node_number += node_number
+                
             except:
                 print("error happened")
-    counts = Counter(nodes)
-    print(counts)
- 
+        print("total is " , total_node_number)
+        nodes.append((link,total_node_number))
+    print(nodes)
+    print("Sorted data : ")
+    print(sorted(nodes, key=lambda tup: (tup[1]) , reverse=True))
+    
+
 
 
 urls = [] 
@@ -34,9 +45,9 @@ driver = webdriver.Chrome(service=ChromeService(
  
 
 
-for i in range(1,10):
+for i in range(1,2):
     driver.get(url+str(i)) 
-    time.sleep(5)
+    time.sleep(8)
     elements = driver.find_elements(By.XPATH ,"//a[@href]")
     href_values = [element.get_attribute("href") for element in elements]
     for href in href_values:
